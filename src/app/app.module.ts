5656307b152605eba4591f1f64ec0a9fdbfac879
stepa <stepan.bukel@gmail.com>
2023-02-16 14:10:12 +0100

    Added players class, add-places component. Changed add places html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { CreateCityGameComponent } from './components/create-city-game/create-city-game.component';
 import { CityGameService } from './services/city-game.service';
 import { FormsModule } from '@angular/forms';
+import { AddPlacesComponent } from './components/add-places/add-places.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateCityGameComponent,
     CreateCityGameComponent,
+    AddPlacesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [CityGameService],
---
+++ src/app/models/players.ts
@@ -0,0 +1,4 @@
+import { GameParticipant } from './game-participant';
+
+export class Players extends GameParticipant{
+}
