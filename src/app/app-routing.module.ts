a2dba8402977b705cb860b2d42b0f24e21415448
stepa <stepan.bukel@gmail.com>
2023-02-16 23:43:37 +0100

    changed styles, add placec component html, added transfer to add places html from create component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCityGameComponent } from './components/create-city-game/create-city-game.component';
+import { AddPlacesComponent } from './components/add-places/add-places.component';
 
 const routes: Routes = [
-  { path: 'createcitygame', component: CreateCityGameComponent },
+  { path: 'createcitygame', component: CreateCityGameComponent }, { path: 'addplaces', component: AddPlacesComponent },
 ];
 
 @NgModule({
--- src/app/components/add-places/add-places.component.ts
+++ src/app/components/add-places/add-places.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CityGameService } from 'src/app/services/city-game.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-places',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-places.component.css']
 })
 export class AddPlacesComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cityGameService: CityGameService
+  ) {}
 
+  onSubmit() {
+    console.log('Hello Add New Place');
+    this.addNewPlace();
+  }
+
+  addNewPlace() {
+    console.log('Hej Hej Hej');
+  }
 }
