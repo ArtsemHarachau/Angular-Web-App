ed6404f056b4d185cda5044e17377ed2f6b5f910
CybArciom <arciom.goriaczew@gmail.com>
2023-02-27 20:30:46 +0100

    Added google maps. Created methods for adding markers to maps, geocoding address and geocoordinates. Handled geocoding errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { AddPlacesComponent } from './components/add-places/add-places.component';
 import { StartComponent } from './components/start/start.component';
 import { GetAllCityGamesComponent } from './components/get-all-city-games/get-all-city-games.component';
+// import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -19,8 +20,40 @@ import { GetAllCityGamesComponent } from './components/get-all-city-games/get-al
     StartComponent,
     GetAllCityGamesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyCjRxKpt1rFfCDXUl0zd1_tqZcvp1ldWhY',
+    // }),
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyCjRxKpt1rFfCDXUl0zd1_tqZcvp1ldWhY',
+    // }),
+  ],
   providers: [CityGameService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+//20 LINE IN PACKAGE-LOCK.JSON
+// "@types/google.maps": "^3.52.0",
+// "@types/googlemaps": "^3.43.3",
+
+//3278 LINE IN PACKAGE-LOCK.JSON
+// "node_modules/@types/google.maps": {
+//   "version": "3.52.0",
+//   "resolved": "https://registry.npmjs.org/@types/google.maps/-/google.maps-3.52.0.tgz",
+//   "integrity": "sha512-cIwkgSBUOCerEwEpAahg1SxUqqGV+D786TkVWrcZZyPvuCozmXFtzQcpOzvUXBtTUqDzEbCDGlAXDfDSYFXFIw=="
+// },
+// "node_modules/@types/googlemaps": {
+//   "version": "3.43.3",
+//   "resolved": "https://registry.npmjs.org/@types/googlemaps/-/googlemaps-3.43.3.tgz",
+//   "integrity": "sha512-ZWNoz/O8MPEpiajvj7QiqCY8tTLFNqNZ/a+s+zTV58wFVNAvvqV4bdGfnsjTb5Cs4V6wEsLrX8XRhmnyYJ2Tdg==",
+//   "deprecated": "Types for the Google Maps browser API have moved to @types/google.maps. Note: these types are not for the googlemaps npm package, which is a Node API."
+// },
+
+//22 LINE IN PACKAGE.JSON
+// "@types/google.maps": "^3.52.0",
+// "@types/googlemaps": "^3.43.3",
--- src/app/components/add-places/add-places.component.ts
+++ src/app/components/add-places/add-places.component.ts
@@ -1,5 +1,11 @@
-import { Component, Renderer2, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  Component,
+  Renderer2,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { ActivatedRoute, ResolveEnd, Router } from '@angular/router';
 import { CityGameService } from 'src/app/services/city-game.service';
 import { NgForm } from '@angular/forms';
 import {} from 'googlemaps';
@@ -11,15 +17,22 @@ import { PlaceInGame } from 'src/app/models/place-in-game';
   styleUrls: ['./add-places.component.css'],
 })
 export class AddPlacesComponent {
+  // ERROR: Geocode was not successful for the following reason: ZERO_RESULTS
+
+  addressInputElem = document.getElementById('address') as HTMLInputElement;
+
+  infoWindow!: google.maps.InfoWindow;
+
+  geocoder!: google.maps.Geocoder;
+  map!: google.maps.Map;
+  possiblePlaceMarker!: google.maps.Marker;
+  private mapsMarkers: google.maps.Marker[] = [];
+
+  possibleAddress: string = '';
+
   private countPlaces: number = 1;
 
   placeInGame: PlaceInGame;
-
-  // @ViewChild('map') mapElement: any;
-  // map: google.maps.Map;
-  // private const center: google.maps.LatLngLiteral = { center: {lat: 30, lng: -110}, zoom: 8, mapId: '1234' } as google.maps.MapOptions;
-
-  // key = AIzaSyCjRxKpt1rFfCDXUl0zd1_tqZcvp1ldWhY
 
   constructor(
     private route: ActivatedRoute,
@@ -30,39 +43,177 @@ export class AddPlacesComponent {
     this.placeInGame = new PlaceInGame();
   }
 
-  // ngOnInit(): void {
-  //   const mapProperties = {
-  //     center: new google.maps.LatLng(35.2271, -80.8431),
-  //     zoom: 15,
-  //     mapTypeId: google.maps.MapTypeId.ROADMAP
-  //   };
+  //THIS IS THE PLACE FOR YOUR FUNCTION
+  //
+  //
 
-  //   this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
-  // }
+  mapInitializer() {
+    this.geocoder = new google.maps.Geocoder();
+
+    let lat = 52.46;
+    let long = 16.94;
+    let coordinates = new google.maps.LatLng(lat, long);
+
+    let mapOptions: google.maps.MapOptions = {
+      center: coordinates,
+      zoom: 8,
+    };
+
+    this.map = new google.maps.Map(document.getElementById('map')!, mapOptions);
+
+    this.map.addListener('click', (mapsMouseEvent) => {
+      if (!!this.possiblePlaceMarker) {
+        this.possiblePlaceMarker.setMap(null);
+      }
+      this.possiblePlaceMarker = new google.maps.Marker({
+        position: mapsMouseEvent.latLng,
+        map: this.map,
+      });
+      this.possiblePlaceMarker.setMap(this.map);
+
+      this.addressFromCoord(mapsMouseEvent.latLng);
+      // addressElem!.innerHTML = mapsMouseEvent.latLng.toString();
+      // console.log(addressElem!.innerHTML);
+      // Create a new InfoWindow.
+      // this.infoWindow = new google.maps.InfoWindow({
+      //   position: mapsMouseEvent.latLng,
+      // });
+      // this.infoWindow.setContent(
+      //   JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+      // );
+      // this.infoWindow.open(this.map);
+    });
+  }
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+
+    // let addressElem = document.getElementById('address') as HTMLInputElement;
+    // addressElem.addEventListener('focusout', (event) => this.addressChanged());
+  }
+
+  async addressChanged() {
+    let addressElem = document.getElementById('address') as HTMLInputElement;
+    console.log('ADDRESS EVENT: -> ', addressElem.value);
+
+    this.geocoder.geocode(
+      { address: addressElem.value },
+      function (results, status) {
+        if (addressElem.value !== '') {
+          if (status == 'OK') {
+            console.log('ADDRESS IS OK!');
+          } else {
+            console.log('ADDRESS IS NOT OK!');
+            alert(
+              'Your address does not exist! Please enter address correctly: ' +
+                status
+            );
+          }
+        }
+      }
+    );
+  }
+
+  async addressFromCoord(coords: google.maps.LatLng) {
+    this.geocoder.geocode({ location: coords }, function (results) {
+      let addressElem = document.getElementById('address') as HTMLInputElement;
+      addressElem.value = results[0].formatted_address;
+    });
+  }
+
+  saveNewPlace(
+    address: string,
+    legend: string,
+    maps: google.maps.Map,
+    placeInGame: PlaceInGame,
+    cityGameService: CityGameService,
+    markers: google.maps.Marker[]
+  ) {
+    let legendElem = document.getElementById('legend') as HTMLTextAreaElement;
+    if (legendElem.value !== '') {
+      console.log("Let's go!");
+
+      let addressElem = document.getElementById('address') as HTMLInputElement;
+      console.log('ADDRESS EVENT: -> ', addressElem.value);
+
+      this.geocoder.geocode(
+        { address: addressElem.value },
+        function (results, status) {
+          if (addressElem.value !== '') {
+            if (status == 'OK') {
+              console.log('ADDRESS IS OK!');
+
+              //create new marker on maps
+              markers.push(
+                new google.maps.Marker({
+                  position: results[0].geometry.location,
+                  label: (markers.length + 1).toString(),
+                  title: results[0].geometry.location.toString(),
+                })
+              );
+
+              markers[markers.length - 1].addListener('click', function (e) {
+                console.log(this.getPosition()?.toJSON());
+                this.setMap(null);
+              });
+
+              markers[markers.length - 1].setMap(maps);
+
+              //preparing placeInGame object for sending to the database
+              placeInGame.orderId = markers.length;
+              placeInGame.latitudeCoord =
+                results[0].geometry.location.toJSON().lat;
+              placeInGame.longitudeCoord =
+                results[0].geometry.location.toJSON().lng;
+              placeInGame.address = address;
+              placeInGame.legend = legend;
+              placeInGame.photoLink = '';
+              placeInGame.cityGame = cityGameService.getCityGameObject();
+
+              console.log(JSON.stringify(placeInGame));
+
+              //send placeInGame object to database
+              cityGameService
+                .addNewPlaceToGame(placeInGame)
+                .subscribe((resp) => {});
+            } else {
+              console.log('ADDRESS IS NOT OK!');
+              alert(
+                'Your address does not exist! Please enter address correctly: ' +
+                  status
+              );
+            }
+          } else {
+            alert('Address is empty!');
+          }
+        }
+      );
+    } else {
+      alert('Legend of place is empty!');
+    }
+  }
 
   onSubmit() {
-    console.log('Hello Add New Place');
+    let addressElem = document.getElementById('address') as HTMLInputElement;
+    let legendElem = document.getElementById('legend') as HTMLInputElement;
 
-    console.log(this.cityGameService.getCityGameObject());
-    console.log(this.placeInGame.address);
-    console.log(this.placeInGame.legend);
+    this.saveNewPlace(
+      addressElem.value,
+      legendElem.value,
+      this.map,
+      this.placeInGame,
+      this.cityGameService,
+      this.mapsMarkers
+    );
 
-    this.placeInGame.orderId = this.countPlaces;
-    this.placeInGame.latitudeCoord = 1.1;
-    this.placeInGame.longitudeCoord = 1.0;
-    this.placeInGame.photoLink = '';
     this.placeInGame.cityGame = this.cityGameService.getCityGameObject();
-
-    this.cityGameService
-      .addNewPlaceToGame(this.placeInGame)
-      .subscribe((resp) => {});
+    console.log(this.cityGameService.getCityGameObject());
 
     this.addNewPlace();
   }
 
   addNewPlace() {
     console.log('Hej Hej Hej');
-    // let newPlaceButton = document.createElement('button');
 
     let newPlaceButton = this.renderer.createElement('button');
     newPlaceButton.innerText = `Place ${this.countPlaces++}`;
@@ -76,14 +227,7 @@ export class AddPlacesComponent {
         document.getElementById('zero-places-span')
       );
     }
-    // parent?.appendChild(newPlaceButton);
+
     this.renderer.appendChild(parent, newPlaceButton);
   }
-
-  // initMap() {
-  //   this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  //     this.center,
-  //     zoom: 8
-  //   });
-  // }
 }
